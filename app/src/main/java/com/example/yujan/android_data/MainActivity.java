37800d6dc4c49617a7e71f8053d04ff4a9b536ec
wangyujian123456 <1063597906@qq.com>
2020-03-13 18:55:09 +0800

    测试事件分发机制（1）

--- app/src/main/java/com/example/yujan/android_data/MainActivity.java
+++ app/src/main/java/com/example/yujan/android_data/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.yujan.android_data;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
+/**
+ * 测试默认情况下 activity/view group/view 事件执行顺序
+ */
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "ceshi--------";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.i(TAG, "MainActivity:dispatchTouchEvent");
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.i(TAG, "MainActivity:onTouchEvent");
+        return super.onTouchEvent(event);
     }
 }
